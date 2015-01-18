710dca2d799c4f986b052976ea09a907d79fe292
David Foster <dfoster89776@icloud.com>
2015-01-18 15:09:16 +0000

    Initial Commit

---
+++ courseshare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  courseshare
+//
+//  Created by David Foster on 18/01/2015.
+//  Copyright (c) 2015 David Foster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
